95a3bb6b8bdd0a20e18aded62a434a007df278dd
even311379 <even311379@hotmail.com>
2023-07-18 00:50:26 +0800

    yt backup improved

--- IFCS/Scripts/youtube_backup/Automation.py
+++ IFCS/Scripts/youtube_backup/Automation.py
@@ -2,29 +2,29 @@ import subprocess
 import sys
 import datetime
 import time
+import os
 
-video_id = "6tj5ZdIJK_o"
+video_id = "zc8XYQnPj-4"
 clip_length = 15
-target_dir = "L:/Free_Dev/YoutubeBackup"
+target_dir = "/home/even/文件/DEV/"
 
 
 if __name__ ==  "__main__":
     print("*"*100)
     print("Press Ctrl + C to stop".center(100, "*"))
-    now = datetime.datetime.now()
-    # need to reserve some seconds for task to start...
-    reserve_time = 45 # reserve 45 second for task to start
-    print('waiting to start...')
-    if now.second < 60 - reserve_time:
-        time.sleep(60 - reserve_time - now.second)
-    else:
-        time.sleep(60 - now.second + 60 - reserve_time)
-    subprocess.Popen(f"{sys.executable} YoutubeDVR_Task.py --clip_length {clip_length} --video_id {video_id} --path {target_dir}")    
-    print('task starts...')
-    time.sleep(60 * (clip_length - now.minute % clip_length) - 5)
+    print("*"*100)
+    clips_recording = 0
     while True:
-        subprocess.Popen(f"{sys.executable} YoutubeDVR_Task.py --clip_length {clip_length} --video_id {video_id} --path {target_dir}")    
-        print('task starts...')
-        time.sleep(60 * clip_length)
+        if clips_recording == 0:
+            subprocess.Popen(f"{sys.executable} YoutubeDVR_Task.py --clip_length {clip_length} --video_id {video_id} --path {target_dir} --start_now", shell=True)    
+            clips_recording += 1
+        else:
+            now = datetime.datetime.now()
+            if now.second == 0:            
+                if (clip_length == 15 and now.minute%15 == 14) or (clip_length == 30 and now.minute%30 == 29) or (clip_length == 60 and now.minute == 59) or \
+                    (clip_length == 120 and now.hour%2 == 1 and now.minute == 59):
+                    subprocess.Popen(f"{sys.executable} YoutubeDVR_Task.py --clip_length {clip_length} --video_id {video_id} --path {target_dir}", shell=True)    
+                    clips_recording += 1
+        time.sleep(1)
         
 
--- IFCS/Scripts/youtube_backup/YoutubeDVR_Task.py
+++ IFCS/Scripts/youtube_backup/YoutubeDVR_Task.py
@@ -8,66 +8,93 @@ def main():
     parser = argparse.ArgumentParser(description="Automate backup youtube streams to your local disk")
     parser.add_argument('--video_id', type=str, help='the video id for your target youtube video. /n \
         Take a random youtube video clip as example: https://www.youtube.com/watch?v=lro-8GUX9Yk the ID is lro-8GUX9Yk')
-    parser.add_argument('--clip_length', type=int, help='the length of each clips you want to store, should be 15, 30, or 60',
-                        choices=[15, 30, 60], default=60)
+    parser.add_argument('--clip_length', type=int, help='the length of each clips you want to store, should be 15, 30, 60, or 120',
+                        choices=[15, 30, 60, 120], default=60)
     parser.add_argument('--path', type=str, help='the absolute path to where you want to store the clips')
+    parser.add_argument('--start_now', dest='start_now', action='store_true', help='start this task without wait')
+    parser.set_defaults(start_now=False)
     args = parser.parse_args()
     if not args.video_id or not args.path:
         print('arguments are missing!')
         return
-    print("start recording...")
     video_id = args.video_id
     clip_length = args.clip_length
     path = args.path
+    start_now = args.start_now
     # stream = CamGear(source='https://youtu.be/' + video_id,stream_mode = True,logging=True, backend=cv2.CAP_GSTREAMER).start() 
-    stream = CamGear(source='https://youtu.be/' + video_id,stream_mode = True,logging=True).start() 
+    stream = CamGear(source='https://youtu.be/' + video_id, stream_mode = True,logging=False, time_delay=1).start() 
     w = int(stream.stream.get(cv2.CAP_PROP_FRAME_WIDTH))
     h = int(stream.stream.get(cv2.CAP_PROP_FRAME_HEIGHT))
     fps = stream.stream.get(cv2.CAP_PROP_FPS)
-    print(f"video info: w {w}, h {h}, fps {fps}")
-    now = datetime.datetime.now()    
-    now = now + datetime.timedelta(seconds=60) # make correct minute in response for reserve time
-    if clip_length == 15:
-        if now.minute < 15:
-            now_string = now.strftime("%Y%m%d-%H%M-%H15")
-        elif now.minute < 30:
-            now_string = now.strftime("%Y%m%d-%H%M-%H30")
-        elif now.minute < 45:    
-            now_string = now.strftime("%Y%m%d-%H%M-%H45")
-        else:
-            now_string = now.strftime("%Y%m%d-%H%M") + f"-{str(now.hour+1).zfill(2)}00"
-    elif clip_length == 30:
-        if now.minute < 30:
-            now_string = now.strftime("%Y%m%d-%H%M-%H30")
-        else:
-            now_string = now.strftime("%Y%m%d-%H%M") + f"-{str(now.hour+1).zfill(2)}00"
+    print(f"stream ready! info: w {w}, h {h}, fps {fps}")
+    start_time = datetime.datetime.now()    
+    start_time = start_time.replace(second=0)
+    if start_time.hour == 23:
+        new_hour = 0
     else:
-        now_string = now.strftime("%Y%m%d-%H%M") + f"-{str(now.hour+1).zfill(2)}00"
+        new_hour = start_time.hour + 1
+    # setup proper start + end time
+    if clip_length == 15:
+        if start_time.minute < 15:
+            start_time = start_time.replace(minute=15)
+        elif start_time.minute < 30:
+            start_time = start_time.replace(minute=30)
+        elif start_time.minute < 45:
+            start_time = start_time.replace(minute=45)
+        else:
+            start_time = start_time.replace(minute=0, hour=new_hour)
+    elif clip_length == 30:
+        if start_time.minute < 30:
+            start_time = start_time.replace(minute=30)
+        else:
+            start_time = start_time.replace(minute=0, hour=new_hour)
+    elif clip_length == 60:
+        start_time = start_time.replace(minute=0, hour=new_hour)
+    elif clip_length == 120:
+        start_time = start_time.replace(minute=0)
+        if start_time.hour >= 22:
+            start_time = start_time.replace(day=start_time.day+1, hour=0)
+        else:
+            if start_time.hour % 2 == 0:
+                new_hour = start_time.hour + 2
+            else:
+                new_hour = start_time.hour + 1
+            start_time = start_time.replace(hour=new_hour)    
+    if start_now:
+        end_time = start_time
+        start_time = datetime.datetime.now()
+    else:
+        end_time = start_time + datetime.timedelta(minutes=clip_length)
+        
 
-    # print("recording streams...")
-    writer = cv2.VideoWriter(f"{path}/{now_string}.mp4", cv2.VideoWriter_fourcc(*'mp4v'), fps, (w, h))
+    time_info = start_time.strftime("%Y%m%d%H%M") + "__" + end_time.strftime("%Y%m%d%H%M")
+    writer = cv2.VideoWriter(f"{path}/{time_info}.mp4", cv2.VideoWriter_fourcc(*'mp4v'), fps, (w, h))
     frames_written = 0
-    minus_minute = now.minute % clip_length
-    start = datetime.datetime.now().second == 0
+    minutes_recorded = 0
+    if start_now:
+        print(f'Start recording... {start_time.strftime("%Y%m%d%H%M")} to {end_time.strftime("%Y%m%d%H%M")}')
+    frames_to_write = fps * (end_time - start_time).seconds
     while True:
-        if not start:
-            time.sleep(1)
-            start = datetime.datetime.now().second == 0
-        if start:
-            frame = stream.read()
-            if frame is None:
-                break
+        frame = stream.read()
+        if frame is None:
+            break
+        if start_now:
             writer.write(frame)
-            key = cv2.waitKey(1) & 0xFF
-            if key == ord("q"):
-                break
-            if frames_written > fps * 60 * (clip_length - minus_minute):
+            if frames_written > frames_to_write:
                 print("one clip is finished".center(50, "*"))
                 break
             if frames_written % (60 * fps) == 0:
-                print("... Recording ... 1 minute passed")
+                print(f"    Recording ... {minutes_recorded}/{(end_time - start_time).seconds//60} minutes")
+                minutes_recorded += 1
             frames_written += 1
-
+        else:
+            now = datetime.datetime.now()
+            if (now.hour >= start_time.hour and now.minute >= start_time.minute and now.second >= start_time.second) or now.day > start_time.day:
+                start_now = True
+                print(f'Start recording... {start_time.strftime("%Y%m%d%H%M")} to {end_time.strftime("%Y%m%d%H%M")}')
+            # else:
+            #     time.sleep(1)
+    print("Recording completed!")
     writer.release()
 
 if __name__ == "__main__":
